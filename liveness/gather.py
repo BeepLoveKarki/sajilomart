4c39c75150a53f1fdb2b07504f2f22e9eafcc19e
jaykishanpanjiyar <43920613+JAY-KISHAN-PANJIYAR@users.noreply.github.com>
2019-07-20 12:08:55 +0545

    liveness

---
+++ liveness/gather.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 20 12:04:23 2019
+
+@author: user
+"""
+
+import numpy as np
+import argparse
+import cv2
+import os
+#path to output directory of cropped faces
+output=""
+#path to OpenCV's deep learning face detector
+detector
