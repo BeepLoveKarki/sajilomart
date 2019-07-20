0f3508fd83063b2009c9ee67e15ebee387673cbc
GairePravesh <praveshgaire.ioe@gmail.com>
2019-07-20 16:03:36 +0545

    Login Page added

---
+++ ui_ux/lib/misc.dart
@@ -0,0 +1,28 @@
+
+    // final emailField = TextField( 
+    //   obscureText: false,
+    //   style: style,
+    //   decoration: InputDecoration( 
+    //     contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+    //     hintText: "Email ID",
+    //     border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))
+    //   )
+    // );
+    // final addressField = TextField( 
+    //   obscureText: false,
+    //   style: style,
+    //   decoration: InputDecoration( 
+    //     contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+    //     hintText: "Address",
+    //     border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))
+    //   )
+    // );
+    // final phoneField = TextField( 
+    //   obscureText: false,
+    //   style: style,
+    //   decoration: InputDecoration( 
+    //     contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+    //     hintText: "Phone Number",
+    //     border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))
+    //   )
+    // );
